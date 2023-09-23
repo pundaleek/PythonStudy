9c22e8a10fdcea7b93eddf15c93f6d1c1e258ee5
Pundaleek B H <pundaleek@prakat.in>
2023-09-24 00:25:16 +0530

    Shri Ganesh Commit

---
+++ src/w3Schools/outputVariables.py
@@ -0,0 +1,27 @@
+# Output Variables
+# The Python print() function is often used to output variables.
+
+# x = "Pythos is awesome"
+# print(x)
+
+# In the print() function, you output multiple variables, separated by a comma:
+x = "Python"
+y = "is"
+z = "awesome"
+# print("x, y, z : "+ x, y, z)
+# print(x+y+z)
+
+x = 100
+y = 200
+z = 300
+# print("x, y, z : "+ x, y, z)
+print(x, y, z)
+print(x+y+z)
+print(x)
+print(y)
+print(z)
+
+# throws exception
+x = 5
+y = "abc"
+print(x + y)
---
+++ src/w3Schools/variables.py
@@ -0,0 +1,45 @@
+# Variables
+num = 5
+name = "Appu"
+# print("value of num is :"+num)
+print("value of name is : "+name)   
+print("num values is : "+str(num))
+
+# Camel Case
+myVariableName = "John"
+print(myVariableName)
+
+# Pascal Case
+MyVariableName = "John"
+print(MyVariableName)
+
+# Snake Case    
+my_variable_name = "John"
+print(my_variable_name)
+
+z = 4
+z = "OK"
+print(z)
+
+# Casting
+x1 = str(3)
+y1 = int(3)
+z1 = float(3)
+print(x1, y1, z1)
+
+# Get the Type
+print(type(x1), type(y1), type(z1))
+
+# Single or Double Quotes
+a = 'abcd'
+ab = "abcd"
+print(type(a), type(ab))
+
+# Case-Sensitive - Varibale Names are Case-Sensitive
+xyz = "4"
+XYZ = "Appu"
+XyZ = "Bijapur"
+# XyZ won't overwrite XYZ/xyz values
+print("xyz : "+xyz)
+print("XYZ : "+XYZ)
+print("XyZ : "+XyZ)
