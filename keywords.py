e968e9c97d7f6e48e5e0d0331829b78e1fa15fd4
biradarkhandu <145952814+biradarkhandu@users.noreply.github.com>
2023-10-17 22:18:52 +0530

    Add files via upload

---
+++ keywords.py
@@ -0,0 +1,39 @@
+a=10
+b=60
+c=a+b
+print(c)
+
+import tkinter
+w=tkinter.Tk()
+w['bg']="cyan"
+w.mainloop()
+
+import tkinter
+w=tkinter.Tk()
+w['bg']="yellow"
+w.mainloop()
+
+import tkinter
+w=tkinter.Tk()
+w['bg']="#20ff40"
+w.mainloop()
+
+import tkinter
+w=tkinter.Tk()
+w['bg']="#ff0000"
+w.mainloop()
+
+import tkinter
+w=tkinter.Tk()
+w['bg']="#ff00ff"
+w.mainloop()
+a=10
+print(type(10))
+
+import math
+print(dir(math))
+
+import os
+print(dir(os))
+
+import 
