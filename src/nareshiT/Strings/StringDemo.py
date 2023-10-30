6ff45a845dc756cbd8e44f84c65180bd2281ed50
Pundaleek B H <pundaleek@prakat.in>
2023-10-30 13:25:59 +0530

    Changes Committed

---
+++ src/nareshiT/Strings/StringDemo.py
@@ -0,0 +1,14 @@
+'''
+# Program to input rollno,name,course
+rollno = int(input("Enter Rollno : "))
+name = input("Enter Name : ")
+course = input("Enter Course: ")
+print(rollno, name, course)
+print(type(rollno), type(name), type(course))
+'''
+
+#WAP to add two numbers
+num1 = float(input("Number 1 : "))
+num2 = float(input("Number 2 : "))
+sum = num1 + num2
+print(sum, '\n', type(sum))
